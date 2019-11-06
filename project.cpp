f406e5d6d726f398a85a3067d2cc73103e6fc461
JessicaMadeira97 <55490248+JessicaMadeira97@users.noreply.github.com>
2019-11-06 15:43:14 +0100

    Attempt #4
    Adding the tempOnDay function to the cpp

--- project.cpp
+++ project.cpp
@@ -3,10 +3,10 @@
 
 //This is the main file
 void project() {
-	string pathToFile = "/home/path/to/file/"; //Put the path to your data file here
-	//tempTrender t(pathToFile); //Instantiate your analysis object
+	string pathToFile = "/home/courseuser/HOME/FINAL_PROJECT/Git_Repo/MNXB01-Climate-Project-Group-G-/smhi-openda_Karlstad.csv"; //Put the path to your data file here
+	tempTrender t(pathToFile); //Instantiate your analysis object
 	
-	//t.tempOnDay(8, 23); //Call some functions that you've implemented
+	t.tempOnDay(8, 23); //Call some functions that you've implemented
 	//t.tempOnDay(235);
 	//t.tempPerDay();
 	//t.hotCold();
--- tempTrender.h
+++ tempTrender.h
@@ -2,20 +2,30 @@
 #define TEMPTRENDER_H
 
 #include <string>
+
 //This is the file that will contain the list of data and function members
 class tempTrender {
 	public:
 	tempTrender(std::string filePath); //Construct using the specified file
 	~tempTrender() {} //Destructor
 	
-	//void tempOnDay(int monthToCalculate, int dayToCalculate); //Make a histogram of the temperature on this day
+	void tempOnDay(int monthToCalculate, int dayToCalculate); //Make a histogram of the temperature on this day
+	void delete_lines(const char *file_name, int n);
+	vector<std::string> split(string s, string delimiter);
+	
+	
 	//void tempOnDay(int dateToCalculate); //Make a histogram of the temperature on this date
 	//void tempPerDay(); //Make a histogram of the average temperature of each day of the year
 	//void hotCold(); //Make a histogram of the hottest and coldest day of the year
 	//void tempPerYear(int yearToExtrapolate); //Make a histogram of average temperature per year, then fit and extrapolate to the given year
 
 	private:
-	
+	int monthToCalculate;
+	int dayToCalculate;
+	const char *file_name;
+	int n;
+	string s;
+	string delimiter;
 };
 
 #endif
