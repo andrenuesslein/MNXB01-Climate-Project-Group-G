0faa019c2cd00ff48a54a87c1ad438bb2e289891
WishIWasBornInTheCreteaceousEra <nat15dil@student.lu.se>
2019-11-07 19:02:47 +0100

    Added Yasser's pruning method to tempOnDay.

--- tempTrender.cpp
+++ tempTrender.cpp
@@ -15,6 +15,8 @@ using namespace std;
 
 //This is the implementation file
 
+
+
 tempTrender::tempTrender(string filePath) {
 	cout << "The user supplied " << filePath << " as the path to the data file." << endl;
 	cout << "You should probably store this information in a member variable of the class. Good luck with the project! :)" << endl;
@@ -22,60 +24,111 @@ tempTrender::tempTrender(string filePath) {
 
 //What does this function do?
 void tempTrender::tempOnDay(int monthToCalculate, int dayToCalculate){
-
-delete_lines("smhi-openda_Karlstad_1.csv", 12);
-ifstream f("smhi-openda_Karlstad_1.csv"); //opening the file for reading
-
-if (f.fail()){
-	cout<<"Could not open file.\n";
-}
-
-TH1D* hist = new TH1D("Histogram","Temperature On a Day Throughout the Years; Temperature[#circC]; Entries", 300, -20,40);
-
-f.ignore(256, '\n');
-char fline[256];
-char delim = '\n';
-char ddel = '-';
-double temp;
-vector<double> temps;
-
-while (f.getline(fline, 256, delim)) 
 {
-vector<string> v = split(fline, ";");
-temp = atof(v.at(2).c_str()); //turns string into double
+	int integer;
+	
+	ifstream myFile("smhi-openda_Karlstad.csv");
 
-vector<string> date = split(v.at(0), "-");
-int year;
-int month;
-int day;
-istringstream(date[0]) >> year;
-istringstream(date[1]) >> month;
-istringstream(date[2]) >> day;
-if (month == monthToCalculate && day == dayToCalculate){
-	temps.push_back(temp);
-	hist->Fill(temp); //Going to become redundant as we make a function to take a vector and fill a histogram.
+	if (!myFile.is_open())
+	{
+		cout << "File is open" << endl;
 	}
-}
-f.close();
 
-TCanvas* c1 = new TCanvas("c1", "Temperature on a Day Throughout the Years", 900, 600);
-double mean = hist->GetMean();
-double stdev = hist->GetRMS();
+	vector<string> year(430000);
+	vector<string> month(430000);
+	vector<string> day(430000);
+	vector<string> time(430000);
+	vector<string> temp(430000);
+	vector<string> test(10);
+	vector<int> x(10);
+	int vectorSize= 430000;
+	int counter = 0;
+	int m=0;
+	
+	TH1D* hist = new TH1D("Histogram","Temperature On a Day Throughout the Years; Temperature[#circC]; Entries", 300, -20,40);
 
-//Set color and the minimum at 0
-hist->SetFillColor(kBlue);
-hist->SetMinimum(0);
+	while (myFile.good())
+	{
 
-// Set ROOT drawing styles
-gStyle->SetOptStat(1111);
-gStyle->SetOptFit(1111);
+		for (int j=0; j<10 ; j++)
+		{
+			myFile.ignore(5000, '\n');
+			
+		}		
+		
+		do
+		{	
+			getline(myFile, test[m], '\n');
+			stringstream geek(test[m]);
+			geek >> x[m];
+			cout << x[m] << endl;
+			m++;
+				
+		}
+		while( x.at(m) != 0 );
+		
+		
+		
+		
+		 	
+	
+		for (int i=0; i < vectorSize ; i++)
+		{
+	 
 
-//Fit it with a Gaussian
-hist->Fit("gaus");
+			getline(myFile, year[i], '-');
+			getline(myFile, month[i], '-');
+			getline(myFile, day[i], ';');
+			getline(myFile, time[i], ';');
+			getline(myFile, temp[i], ';');
+			myFile.ignore(400, '\n');
+			
+				double y=0.;
+				stringstream yeek(month.at(i));
+				yeek >> y;
+			
+				double z=0.;
+				stringstream zeek(day.at(i));
+				zeek >> z;
+				
+			if (y == monthToCalculate && z == dayToCalculate){
+				double x=0.;
+				stringstream geek(temp.at(i));
+				geek >> x;
+				hist->Fill(x); //Going to become redundant as we make a function to take a vector and fill a histogram.
+			}
 
-//Draw it and save it
-hist->Draw();	
-c1->SaveAs("TempOnDayHist.jpg");	
+		}
+	
+		
+	}
+
+	TCanvas* c1 = new TCanvas("c1", "Temperature on a Day Throughout the Years", 900, 600);
+	double mean = hist->GetMean();
+	double stdev = hist->GetRMS();
+
+
+	//Style Settings
+	gStyle->SetOptStat(0); //Let's make our plots look a bit better. Get rid of the stat box
+	gStyle->SetOptTitle(0); //Get rid of the title (good plots use legends instead)
+	gStyle->SetTitleSize(0.05, "x"); //Use bigger text on the axes
+	gStyle->SetTitleSize(0.05, "y");
+	gStyle->SetLabelSize(0.05, "x"); //Use bigger labels too
+	gStyle->SetLabelSize(0.05, "y");
+	gStyle->SetPadTopMargin(0.05); //Change the margins to fit our new sizes
+	gStyle->SetPadRightMargin(0.05);
+	gStyle->SetPadBottomMargin(0.16);
+	gStyle->SetPadLeftMargin(0.16);
+	
+	//Set color and the minimum at 0
+	hist->SetFillColor(kBlue);
+	hist->SetMinimum(0);
+	//Fit it with a Gaussian
+	hist->Fit("gaus");
+
+	//Draw it and save it
+	hist->Draw();	
+	c1->SaveAs("TempOnDayHist.jpg");	
 }
 
 vector<std::string> tempTrender::split(string s, string delimiter){
